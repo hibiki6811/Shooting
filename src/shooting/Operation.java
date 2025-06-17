95a34dfaa6f8d5a508979427fe481ca6362f6e45
User <User@192.168.10.113>
2025-06-18 00:41:32 +0900

    c

--- src/shooting/Operation.java
+++ src/shooting/Operation.java
@@ -50,6 +50,23 @@ public class Operation implements KeyListener{
 	        case KeyEvent.VK_SPACE:
 	        	isSpacePressed = true;
 	        	break;
+	        /*
+			case KeyEvent.VK_W:
+	        	isUpPressed = true;
+	        	break;
+	        case KeyEvent.VK_S:
+	        	isDownPressed = true;
+	        	break;
+	        case KeyEvent.VK_A:
+	        	isLeftPressed = true;
+	        	break;
+	        case KeyEvent.VK_D:
+	        	isRightPressed = true;
+	        	break;
+	        case KeyEvent.VK_SPACE:
+	        	isSpacePressed = true;
+	        	break;
+	        */
 		}
 	}
 
@@ -72,52 +89,64 @@ public class Operation implements KeyListener{
 	        case KeyEvent.VK_SPACE:
 	        	isSpacePressed = false;
 	        	break;
+		 	/*
+		 	case KeyEvent.VK_W:
+	        	isUpPressed = false;
+	        	break;
+	        case KeyEvent.VK_S:
+	        	isDownPressed = false;
+	        	break;
+	        case KeyEvent.VK_A:
+	        	isLeftPressed = false;
+	        	break;
+	        case KeyEvent.VK_D:
+	        	isRightPressed = false;
+	        	break;
+	        case KeyEvent.VK_SPACE:
+	        	isSpacePressed = false;
+	        	break;
+	        */
 	    }
 	}
 	
 	final void operation(Player player) {
-		/*
-		 if(pressedKeyCode == KeyEvent.VK_UP) {
-			player.setIsMovingUp(true);
-		}
-		 if(pressedKeyCode == KeyEvent.VK_DOWN) {
-			  player.setIsMovingDown(true);
-		}*/
-		 if(pressedKeyCode == KeyEvent.VK_LEFT) {
-			 player.setIsMovingLeft(true);
-			 player.setIsMovingRight(false);
-			 if(releasedKeyCode == KeyEvent.VK_LEFT) {
-				 player.setIsMovingLeft(false);
-				 if(isRightPressed) {
-					 pressedKeyCode = KeyEvent.VK_RIGHT;
-					 releasedKeyCode = NO_PRESSED_KEY;
-				 }else {
-					 pressedKeyCode = NO_PRESSED_KEY;
-					 releasedKeyCode = NO_PRESSED_KEY;
-				 }
-				 
-			 }else {
-				 releasedKeyCode = NO_PRESSED_KEY;
-			 }
-		}
-		 if(pressedKeyCode == KeyEvent.VK_RIGHT) {
-			 player.setIsMovingRight(true);
+		 if(pressedKeyCode == KeyEvent.VK_UP && !isDownPressed && !isLeftPressed && !isRightPressed) {
+			 player.setIsMovingUp(isUpPressed);
+		 }
+		 if(releasedKeyCode == KeyEvent.VK_UP) {
+			 player.setIsMovingUp(false);
+			 releasedKeyCode = NO_PRESSED_KEY;
+		 }
+		 if(pressedKeyCode == KeyEvent.VK_DOWN && !isUpPressed && !isLeftPressed && !isRightPressed) {
+			 player.setIsMovingDown(isDownPressed);
+		 }
+		 if(releasedKeyCode == KeyEvent.VK_DOWN) {
+			 player.setIsMovingDown(false);
+			 releasedKeyCode = NO_PRESSED_KEY;
+		 }
+		 if(pressedKeyCode == KeyEvent.VK_LEFT && !isUpPressed && !isDownPressed && !isRightPressed) {
+			 player.setIsMovingLeft(isLeftPressed);
+		 }
+		 if(releasedKeyCode == KeyEvent.VK_LEFT) {
 			 player.setIsMovingLeft(false);
-			 if(releasedKeyCode == KeyEvent.VK_RIGHT) {
-				 player.setIsMovingRight(false);
-				 if(isLeftPressed) {
-					 pressedKeyCode = KeyEvent.VK_LEFT;
-					 releasedKeyCode = NO_PRESSED_KEY;
-				 }else {
-					 pressedKeyCode = NO_PRESSED_KEY;
-					 releasedKeyCode = NO_PRESSED_KEY;
-				 }
-			 }else {
-				 releasedKeyCode = NO_PRESSED_KEY;
-			 }
-		}
+			 releasedKeyCode = NO_PRESSED_KEY;
+		 }
+		 if(pressedKeyCode == KeyEvent.VK_RIGHT && !isUpPressed && !isDownPressed && !isLeftPressed) {
+			 player.setIsMovingRight(isRightPressed);
+		 }
+		 if(releasedKeyCode == KeyEvent.VK_RIGHT) {
+			 player.setIsMovingRight(false);
+			 releasedKeyCode = NO_PRESSED_KEY;
+		 }
+		 if(pressedKeyCode == KeyEvent.VK_SPACE) {
+			 player.setIsFire(isSpacePressed);
+		 }
+		 if(releasedKeyCode == KeyEvent.VK_SPACE) {
+			 player.setIsFire(false);
+			 releasedKeyCode = NO_PRESSED_KEY;
+		 }
 		/*
-	    player.setIsMovingUp(upPressed);
+	    player.setIsMovingUp(isUpPressed);
 	    player.setIsMovingDown(isDownPressed);
 	    player.setIsMovingLeft(isLeftPressed);
 	    player.setIsMovingRight(isRightPressed);
